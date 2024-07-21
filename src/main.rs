6f62d6223c0ec824b13aa5e4aa9fd3c8307ba1fa
norara <792167485@qq.com>
2024-07-21 13:45:45 +0800

    changes to rsrand

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,64 @@
 extern crate clap;
-use clap::{Arg, App, SubCommand};
-use uuid::Uuid;
 
+use std::{fs::File, io::Read};
+
+use clap::{App, YamlLoader};
+use uuid::Uuid;
+use yaml_rust::Yaml;
+
+fn load(path: &str) -> Yaml {
+    let mut f = File::open(path).expect("Failed to open config YAML file");
+    let mut yaml_str = String::new();
+    
+    f.read_to_string(&mut yaml_str).expect("Failed to read config YAML file");
+
+
+    let yaml = YamlLoader::load_from_str(&yaml_str)
+        .expect("Failed to load config YAML file");
+
+    yaml[0].clone()
+}
 fn main() {
-    let matches = App::new("rs uuid generator")
-                                            .version("1")
-                                            .author("norayya")
-                                            .about("a random uuid generator.")
-                                            .subcommand(SubCommand::with_name("new")
-                                                                    .about("create new uuid string")
-                                                                    .version("1")
-                                                                    .author("norayya")
-                                                                    .arg(Arg::with_name("count")
-                                                                            .short("c")
-                                                                            .long("count")
-                                                                            .takes_value(true)
-                                                                            .required(false)
-                                                                            .default_value("1")
-                                                                            .help("how many uuids do you want?")
-                                                                        )
-                                                        )
-                                            .subcommand(SubCommand::with_name("try")
-                                                                    .about("parse a string and check whether it is a uuid")
-                                                                    .version("1")
-                                                                    .author("norayya")
-                                                                    .arg(Arg::with_name("uuid")
-                                                                            .short("u")
-                                                                            .long("uuid")
-                                                                            .takes_value(true)
-                                                                            .required(true)
-                                                                            .help("uuid string")
-                                                                )
-                                                        )
-                                            .get_matches();
+    let yaml = load("cli.yaml");
+    let matches = App::from_yaml(&yaml).get_matches();
 
     match matches.subcommand() {
-        ("new", Some(matches)) => {
-            if matches.is_present("count"){
-                let count:Result<i32, _>= matches.value_of("count").unwrap().parse();
-                match count {
-                    Ok(num) => {
-                        for _ in 0..num {
-                            let uuid = Uuid::new_v4();
-                            println!("{uuid}");
+        ("uuid", Some(matches)) => {
+            match matches.subcommand() {
+                ("new", Some(matches)) => {
+                    if matches.is_present("count"){
+                        let count:Result<i32, _>= matches.value_of("count").unwrap().parse();
+                        match count {
+                            Ok(num) => {
+                                for _ in 0..num {
+                                    let uuid = Uuid::new_v4();
+                                    println!("{uuid}");
+                                }
+                            },
+                            Err(err) => println!("some error has occurred : {}", err),
                         }
-                    },
-                    Err(err) => println!("some error has occurred : {}", err),
-                }
-            }else{
-                println!("cannot take any args.. failed.");
+                    }else{
+                        println!("cannot take any args.. failed.");
+                    }
+                },
+                ("try", Some(matches))=>{
+                    if matches.is_present("uuid"){
+                        let parse_result = Uuid::try_parse(matches.value_of("uuid").unwrap());
+                        match parse_result {
+                            Ok(_) => println!("success"),
+                            Err(err) => println!("some error has occurred : {}", err),
+                        }
+        
+                    }else{
+                        println!("cannot take any args.. failed.")
+                    }
+                },
+                _ => println!("command not found.. failed.")
             }
-        },
-        ("try", Some(matches))=>{
-            if matches.is_present("uuid"){
-                let parse_result = Uuid::try_parse(matches.value_of("uuid").unwrap());
-                match parse_result {
-                    Ok(_) => println!("success"),
-                    Err(err) => println!("some error has occurred : {}", err),
-                }
-
-            }else{
-                println!("cannot take any args.. failed.")
-            }
+        
         },
         _ => println!("command not found.. failed.")
     }
+
 
 }
